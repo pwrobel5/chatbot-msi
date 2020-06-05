6580ecf7eb112a537289f7199ed8837ebdc3a635
Piotr Wróbel <pwrobel5@gmail.com>
2020-06-05 23:31:00 +0200

    next correction

--- nlp/get_data.py
+++ nlp/get_data.py
@@ -10,7 +10,7 @@ first_text = response.text.replace("\t", " ").replace("\r", " ").replace("\n", "
 response = requests.get("https://wolnelektury.pl/media/book/txt/homer-odyseja.txt")
 second_text = response.text.replace("\t", " ").replace("\r", " ").replace("\n", " ").split(".")[BEGIN_FROM:(BEGIN_FROM + NUM_OF_SENTENCES_TAKEN)]
 
-with open("text_set.csv", "w") as csvfile:
+with open("text_set.csv", "w", encoding="utf-8") as csvfile:
     csv_writer = csv.writer(csvfile, delimiter="|")
     csv_writer.writerow(["label", "text"])
     for sentence in first_text:
