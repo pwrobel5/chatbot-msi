ec4da4b76e0ed09936de9219cf1619c5f805796d
Piotr Wróbel <pwrobel5@gmail.com>
2020-06-05 23:22:41 +0200

    Corrected formatting of data set

--- nlp/get_data.py
+++ nlp/get_data.py
@@ -5,10 +5,10 @@ BEGIN_FROM = 50
 NUM_OF_SENTENCES_TAKEN = 1000
 
 response = requests.get("https://wolnelektury.pl/media/book/txt/ogniem-i-mieczem-tom-pierwszy.txt")
-first_text = response.text.replace("\t", " ").replace("\n", " ").split(".")[BEGIN_FROM:(BEGIN_FROM + NUM_OF_SENTENCES_TAKEN)]
+first_text = response.text.replace("\t", " ").replace("\r", " ").replace("\n", " ").split(".")[BEGIN_FROM:(BEGIN_FROM + NUM_OF_SENTENCES_TAKEN)]
 
 response = requests.get("https://wolnelektury.pl/media/book/txt/homer-odyseja.txt")
-second_text = response.text.replace("\t", " ").replace("\n", " ").split(".")[BEGIN_FROM:(BEGIN_FROM + NUM_OF_SENTENCES_TAKEN)]
+second_text = response.text.replace("\t", " ").replace("\r", " ").replace("\n", " ").split(".")[BEGIN_FROM:(BEGIN_FROM + NUM_OF_SENTENCES_TAKEN)]
 
 with open("text_set.csv", "w") as csvfile:
     csv_writer = csv.writer(csvfile, delimiter="|")
