b4c8fc6807e26b7a3e21d37044f6ed6f83b01232
Piotr Wróbel <pwrobel5@gmail.com>
2020-06-05 23:18:20 +0200

    Report fragment added, modified text set

--- nlp/get_data.py
+++ nlp/get_data.py
@@ -10,8 +10,8 @@ first_text = response.text.replace("\t", " ").replace("\n", " ").split(".")[BEGI
 response = requests.get("https://wolnelektury.pl/media/book/txt/homer-odyseja.txt")
 second_text = response.text.replace("\t", " ").replace("\n", " ").split(".")[BEGIN_FROM:(BEGIN_FROM + NUM_OF_SENTENCES_TAKEN)]
 
-with open("text_set.csv", "w", newline="") as csvfile:
-    csv_writer = csv.writer(csvfile, delimiter=";")
+with open("text_set.csv", "w") as csvfile:
+    csv_writer = csv.writer(csvfile, delimiter="|")
     csv_writer.writerow(["label", "text"])
     for sentence in first_text:
         csv_writer.writerow(["Ogniem i mieczem", sentence])
